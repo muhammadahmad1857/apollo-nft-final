58b460ddbc8c41d23dbe95f845ec6ce489e00763
= <ahmedjawad1857@gmail.com>
2026-01-25 17:39:21 +0500

    Initialized repository

---
+++ src/components/provider.tsx
@@ -0,0 +1,43 @@
+"use client"
+import React from 'react'
+import { config } from "@/lib/wagmi";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
+const queryClient = new QueryClient();
+
+
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+const Provider = ({children}:{children:React.ReactNode}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <WagmiProvider config={config}>
+      <RainbowKitProvider>
+         
+     <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+        <Toaster richColors closeButton />
+
+          {children}
+        </ThemeProvider>
+          
+          </RainbowKitProvider>
+        </WagmiProvider>
+      </QueryClientProvider>
+  )
+}
+
+export default Provider
---
+++ src/lib/wagmi/index.ts
@@ -0,0 +1,10 @@
+"use client";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { apolloMainnet } from "./apollo-chain";
+
+export const config = getDefaultConfig({
+  appName: "NFT Minting Studio",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "nft-studio",
+  chains: [apolloMainnet],
+  ssr: false,
+});
