ff9302069a1841134d342b56a4421bac799982d3
= <ahmedjawad1857@gmail.com>
2026-01-25 19:46:16 +0500

    removed main code to understand the error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>
+          hello from child
           {children}
         </Provider>
       </body>
