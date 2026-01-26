58a35f1d9e64679710013ead44e036684992ce77
= <ahmedjawad1857@gmail.com>
2026-01-26 23:13:38 +0500

    Wrapped logo ans sidebarTrigger in one div

--- src/components/sidebar-header.tsx
+++ src/components/sidebar-header.tsx
@@ -11,12 +11,14 @@ const SidebarHeader = () => {
   return (
       <header className=" bg-background/80 backdrop-blur-md border-b border-border">
                 <div className="w-full mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-2 px-4">
       <SidebarTrigger className="-ml-1" />
       <Logo show={state==="collapsed"}/>
                     <Separator
                       orientation="vertical"
                       className="mr-2 data-[orientation=vertical]:h-4"
                     />
+                    </div>
         <div className="flex items-center gap-2 px-4">
                     
                   
