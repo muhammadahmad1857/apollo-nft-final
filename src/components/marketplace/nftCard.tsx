7c3ece925b73ef660731cda4387b00fa44872552
= <ahmedjawad1857@gmail.com>
2026-02-10 21:06:47 +0500

    feat: update NFTCard to display highest bid if available, otherwise show mint price

--- src/components/marketplace/nftCard.tsx
+++ src/components/marketplace/nftCard.tsx
@@ -167,7 +167,7 @@ const NFTCard = ({
             <p className="text-sm text-zinc-600 dark:text-zinc-400 mb-3">
               Price:{" "}
               <span className="font-semibold">
-                {mintPrice.toFixed(4)} APOLLO
+                {(auction?.highestBid && auction?.highestBid.toFixed(4)) || mintPrice.toFixed(4)} APOLLO
               </span>
             </p>
           )}
